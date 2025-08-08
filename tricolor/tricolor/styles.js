6752b8fa4e0f715f2ee989ed074f52efa6ed6431
ROGERIO DA SILVA ALCANTARA <sn1024493@educ123.sp.senai.br>
2025-08-08 10:16:27 -0300

    Add app tricolor

---
+++ tricolor/tricolor/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    area1: {
+        flex: 2,
+        backgroundColor: '#25ab33',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    area2: {
+        flex: 1,
+        backgroundColor: '#dd9900',
+        alignItems: 'center',
+        justifyContent: 'center'
+        
+    },
+    area3: {
+        flex: 4,
+        backgroundColor: '#2245bc',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    title: {
+        fontSize: 50,
+        fontWeight: 'bold',
+        //fontStyle: 'italic',
+        color: '#ffffff'
+    }
+});
